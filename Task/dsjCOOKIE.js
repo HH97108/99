0e2ef03a8afd8168a693646c35b0fae827330550
HH97108 <62311089+HH97108@users.noreply.github.com>
2021-07-08 14:14:12 +0800

    Update dsjCOOKIE.js

--- Task/dsjCOOKIE.js
+++ Task/dsjCOOKIE.js
@@ -12,7 +12,7 @@ boxjs链接  https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/ziye.boxjs
 
 
 
-let cookie =
+let cookie = {"dsjheader":"6461c5fe3d969f941fdc3455600daca8&WXpBek9EZGhOV05sWkdWaE1HSmxOemMzTWpjellUWmlOV1prTnpjeE5XUT18MTYyNTcyMzQyNjI0NDY0NjA5MHwxZDQ1YTAwNjg4Mjc1ZDdmZmIwMmI0NDg5YTEwODU1MWQ0YmU5MWM4","dsjurl":"","dsjheader2":"2b6452696881d5cb11cd73b67439ca82&TXpsaVpERmtPRFZpT0dVM1lUTXhZMlF4WXpZd1pUbGxaR1V4WkRsak5tVT18MTYyNTcyNDE4ODc3OTc0NjYxNXxiZmI2MWM1MDcwMDM1ZGZkZDQ4OTFjN2E4M2E5MmQwNzRkMWI0MTlk","dsjurl2":"","dsjheader3":"","dsjurl3":"","dsjheader4":"","dsjurl4":"","dsjheader5":"","dsjurl5":"","dsjheader6":"","dsjurl6":"","dsjheader7":"","dsjurl7":"","dsjheader8":"","dsjurl8":"","dsjheader9":"","dsjurl9":"","dsjheader10":"","dsjurl10":"","dsjheader11":"","dsjurl11":"","dsjheader12":"","dsjurl12":"","dsjheader13":"","dsjurl13":"","dsjheader14":"","dsjurl14":"","dsjheader15":"","dsjurl15":"","dsjheader16":"","dsjurl16":"","dsjheader17":"","dsjurl17":"","dsjheader18":"","dsjurl18":"","dsjheader19":"","dsjurl19":"","dsjheader20":"","dsjurl20":""}
 
 
 
